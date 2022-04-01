8b556d3b15a16be3f20a0779b420a78106392240
Sagar Bandari <sagearonn@gmail.com>
2022-04-01 16:44:33 +0530

    Refactoring code using partials

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,57 @@
+class ArticlesController < ApplicationController
+  
+  before_action :set_articles, only: [:show, :edit, :update, :destroy]
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    # byebug
+    
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      flash[:notice] = "Article was successfully saved"     
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  
+  end
+
+  def update
+    
+    if  @article.update(articles_params)
+      flash[:notice] = "Article was updated"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_articles
+    @article = Article.find(params[:id])
+  end
+
+  def articles_params
+    params.require(:article).permit(:title, :description)
+  end
+end
