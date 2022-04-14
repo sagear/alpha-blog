6244dbddbe8355f348e8344806367004d3291425
Sagar Bandari <sagearonn@gmail.com>
2022-04-14 11:49:56 +0530

    Added user sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root'pages#home'
   get '/about', to: 'pages#about'
   resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
+
 end
