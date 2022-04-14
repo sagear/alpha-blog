aad13ffd53053d53ef87d2602d2392a1e48e9d36
Sagar Bandari <sagearonn@gmail.com>
2022-04-14 12:26:40 +0530

    Added user edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_param)
+    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the Alpha blog #{@user.username}, you have successfully"
       redirect_to articles_path
@@ -14,9 +14,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account is successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
   private
 
-  def user_param
+  def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 end
