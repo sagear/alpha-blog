579fefeb5a709f66929276013f72d789f864bd57
Sagar Bandari <sagearonn@gmail.com>
2022-04-18 11:06:14 +0530

    Added pagination and modified custom css

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,10 @@ class ArticlesController < ApplicationController
   
   before_action :set_articles, only: [:show, :edit, :update, :destroy]
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 3)
   end
 
   def show
-    # byebug
     
   end
   
