8fb71fa05973e9696e8674029a44f18c99ce763f
Sagar Bandari <sagearonn@gmail.com>
2022-04-19 11:11:54 +0530

    Added helper methods for current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
-  
+
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
   end
+  
   def new
     @user = User.new
   end
@@ -10,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha blog #{@user.username}, you have successfully"
       redirect_to articles_path
     else
@@ -17,17 +25,10 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
-    @user = User.find(params[:id])
-    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
-  end
-
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account is successfully updated"
       redirect_to @user
@@ -36,6 +37,10 @@ class UsersController < ApplicationController
     end
   end
   private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
