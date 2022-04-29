bef98d0f97545fa582b8609a95177af2279aa41a
Sagar Bandari <sagearonn@gmail.com>
2022-04-29 13:18:38 +0530

    Added categories and has many associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 6, maximum: 60}
   validates :description, presence: true, length: { minimum: 10, maximum: 300}
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
